b4c7c6cde56fbf3ea3dbef35b4a2733729a05f29
Jonathan Nakagawa <jnakagawa17@gmail.com>
2019-08-29 20:53:57 -0400

    Added routes to the api to handle specific user requests, updates, and deletes

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -7,6 +7,34 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) =>{
+    Exercise.findById(req.param.id)
+        .then(exercise => (res.json(exercise)))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) =>{
+    Exercise.findByIdAndDelete(req.param.id)
+        .then(() => res.json('Exercise deleted'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) =>{
+    Exercise.findById(req.param.id)
+        .then(exercise => {
+            exercise.username = req.body.username;
+            exercise.description = req.body.description;
+            exercise.duration = Number(req.body.duration);
+            exercise.date = Date.parse(req.body.date);
+
+            exercise.save()
+                .then(() => res.json('Exercise updated'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
 router.route('/add').post((req, res) => {
     const username = req.body.username;
     const description = req.body.description;
@@ -19,5 +47,7 @@ router.route('/add').post((req, res) => {
         .then(() => res.json('Exercise Added'))
         .catch(err => res.status(400).json('Error: ' + err));
 });
+
+
 
 module.exports = router;
