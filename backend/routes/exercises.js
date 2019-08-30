6381c4676d9bbe043d75a93f25641ec1db4c692b
Jonathan Nakagawa <jnakagawa17@gmail.com>
2019-08-29 20:59:06 -0400

    Bug fixes

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -7,20 +7,20 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/:id').get((req, res) =>{
-    Exercise.findById(req.param.id)
-        .then(exercise => (res.json(exercise)))
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => res.json(exercise))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/:id').delete((req, res) =>{
-    Exercise.findByIdAndDelete(req.param.id)
+    Exercise.findByIdAndDelete(req.params.id)
         .then(() => res.json('Exercise deleted'))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/update/:id').post((req, res) =>{
-    Exercise.findById(req.param.id)
+    Exercise.findById(req.params.id)
         .then(exercise => {
             exercise.username = req.body.username;
             exercise.description = req.body.description;
